238880a3bacc8b9ce75563eff6ecd640809d7cd8
Sajeel-Awan <maliksajeelawan67@outlook.com>
2024-07-06 00:53:12 +0500

    Add project files.

---
+++ JokeWebApplication/Models/Joke.cs
@@ -0,0 +1,21 @@
+﻿namespace JokeWebApplication.Models
+{
+    public class Joke
+    {        // Properties for the joke question and answer
+        
+
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        // Constructor to initialize the joke
+        public Joke()
+        {
+ 
+        }
+
+     
+
+    }
+}
+
